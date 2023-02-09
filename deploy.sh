0996e9736aa7e37e68472915e04b18803f3f0657
xing <paradisehit@gmail.com>
2023-02-10 00:13:18 +0800

    add deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+set -x
 WORK_DIR=`dirname $(readlink -f $0)`
 echo $WORK_DIR
+git pull
 sudo cp $WORK_DIR/* -R /usr/share/nginx/html/artist/
 sudo chown -R nginx:nginx /usr/share/nginx/html/artist/
