5bf7794bb66e4912d88e83a0a3f7c6bddc78d771
xing <paradisehit@gmail.com>
2023-02-14 23:15:40 +0800

    auto cp sde.js sd.js, sde needs to be update manually https://obfuscator.io/

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
  # @Author: xing paradisehit@gmail.com
  # @Date: 2023-02-10 00:13:37
  # @LastEditors: xing paradisehit@gmail.com
- # @LastEditTime: 2023-02-14 23:13:48
+ # @LastEditTime: 2023-02-14 23:15:35
  # @FilePath: /web/deploy.sh
  # @Description: deploy on server
  # Copyright (c) 2023 by xing shi email: paradisehit@gmail.com, All Rights Reserved.
@@ -13,5 +13,6 @@ WORK_DIR=`dirname $(readlink -f $0)`
 echo $WORK_DIR
 git pull
 sudo cp $WORK_DIR/* -R /usr/share/nginx/html/artist/
+#https://obfuscator.io/
 sudo cp /usr/share/nginx/html/artist/sde.js /usr/share/nginx/html/artist/sd.js 
 sudo chown -R nginx:nginx /usr/share/nginx/html/artist/
